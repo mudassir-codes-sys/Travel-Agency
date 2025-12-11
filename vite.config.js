b3fb9e97e6d55c53d8662a1a7a5f5628575bc34e
mudassir-codes-sys <levelscure@gmail.com>
2025-12-11 16:13:20 +0500

    initial

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ],
-  server: {
-    host: true, // 0.0.0.0 ke barabar
-    port: 5173, // default Vite port, change kar sakte ho
-  },
+  plugins: [react()],
 });
