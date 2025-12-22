de8b8bce6ffe154541135ff991333f411314f66f
mudassir-codes-sys <levelscure@gmail.com>
2025-12-22 22:10:37 +0500

    main added

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
-import { Profiler, StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import "./App.css"
-import App from './App.jsx'
+import { Profiler, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./App.css";
+import App from "./App.jsx";
 function onRender(id, phase, actualDuration, baseDuration) {
   console.log(id, phase, actualDuration, baseDuration);
-
 }
 
-createRoot(document.getElementById('root')).render(
-
+createRoot(document.getElementById("root")).render(
   <Profiler onRender={onRender}>
     <App />
   </Profiler>
-
-
-)
+);
